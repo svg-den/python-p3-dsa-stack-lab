36777de3e05600a5dd997b3b6bc6be6da6a8928a
svg-den <sundaywanjiru@gmail.com>
2024-03-13 17:16:03 +0300

    stack.py lab done

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,43 @@
 class Stack:
 
-    def __init__(self, items = [], limit = 100):
-        pass
+    def __init__(self, items=None, limit=100):
+        if items is None:
+            items = []
+        self.items = items
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
 
     def push(self, item):
-        pass
+        if len(self.items) < self.limit:
+            self.items.append(item)
+        else:
+            print("Stack is full")
 
     def pop(self):
-        pass
+        if not self.isEmpty():
+            return self.items.pop()
+        else:
+            print("Stack is empty")
 
     def peek(self):
-        pass
-    
+        if not self.isEmpty():
+            return self.items[-1]
+        else:
+            raise Exception("Stack is empty")
+
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        return len(self.items) == self.limit
+
+    def empty(self):
+        return len(self.items) == 0
 
     def search(self, target):
-        pass
+        if target in self.items:
+            return len(self.items) - self.items.index(target) - 1
+        else:
+            return -1
